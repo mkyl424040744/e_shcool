1f6b9c905feb4403d7be317982006838a07c3191
mkyl424040744 <424040744@qq.com>
2020-04-27 22:48:39 +0800

    Initial Commit

---
+++ data/data.js
@@ -0,0 +1,38 @@
+var data = [{
+   name: "快捷电话",
+   details: "10分钟内专家回电",
+   // iclass:"iconfont icon-yisheng",
+   imageUrl:"../../images/phone.png",
+   id:0
+}, {
+   name: "医药报销",
+   details: "医保卡报销",
+   imageUrl:"../../images/yibao.png",
+   id:4
+}, {
+   name: "病况咨询",
+   details: "找名医 更准更放心",
+   imageUrl:"../../images/yishen.png",
+   id:1
+}, {
+   name: "门诊队列",
+   details: "10分钟内专家回电",
+   imageUrl:"../../images/duilie.png",
+   id:2
+}, {
+   name: "药品选购",
+   details: "10分钟内专家回电",
+   imageUrl:"../../images/yaowan.png",
+   id:3
+}, {
+   name: "健康科普",
+      details: "10分钟内专家回电",
+      imageUrl:"../../images/kepu.png",
+   id:5
+}]
+
+
+
+module.exports = {
+   data: data
+}
