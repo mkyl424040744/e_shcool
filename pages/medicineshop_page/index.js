d37753c6da6b34f5dc8987b2d382a6cf735e7d45
mkyl424040744 <424040744@qq.com>
2020-06-15 16:24:26 +0800

    20206151624
    修改了药品选购

--- pages/medicineshop_page/index.js
+++ pages/medicineshop_page/index.js
@@ -5,7 +5,12 @@ Page({
     * 页面的初始数据
     */
    data: {
-      nevid: 0
+      nevid: 0,
+      TabCur: 0,
+      MainCur: 0,
+      VerticalNavTop: 0,
+      load: true,
+      list:[]
    },
    scroll: function (e) {
       let id = e.currentTarget.dataset.id
@@ -20,11 +25,49 @@ Page({
          url: 'drug_particulars/index?id='+id,
       })
    },
-   /**
-    * 生命周期函数--监听页面加载
-    */
+   tabSelect(e) {
+      this.setData({
+         TabCur: e.currentTarget.dataset.id,
+         MainCur: e.currentTarget.dataset.id,
+         VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+      })
+   },
+   VerticalMain(e) {
+      let that = this;
+      let list = this.data.list;
+      let tabHeight = 0;
+      if (this.data.load) {
+         for (let i = 0; i < list.length; i++) {
+            let view = wx.createSelectorQuery().select("#main-" + list[i].id);
+            view.fields({
+               size: true
+            }, data => {
+               list[i].top = tabHeight;
+               tabHeight = tabHeight + data.height;
+               list[i].bottom = tabHeight;
+            }).exec();
+         }
+         that.setData({
+            load: false,
+            list: list
+         })
+      }
+      let scrollTop = e.detail.scrollTop + 20;
+      for (let i = 0; i < list.length; i++) {
+         if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+            that.setData({
+               VerticalNavTop: (list[i].id - 1) * 50,
+               TabCur: list[i].id
+            })
+            return false
+         }
+      }
+   },
    onLoad: function (options) {
-      
+      wx.showLoading({
+         title: '加载中...',
+         mask: true
+      });
       this.setData({
          height: wx.getSystemInfoSync().windowHeight,　　//屏幕高度
          width: wx.getSystemInfoSync().windowWidth　　//屏幕宽度
@@ -35,9 +78,19 @@ Page({
          data: {},
          success: res => {
             console.log(res.data)
+            let data = res.data
+            let list = [{}];
+            for (let i = 0; i < data.length; i++) {
+               list[i] = {};
+               // list[i].name = String.fromCharCode(65 + i);
+               list[i].name = i;
+               list[i].id = i;
+            }
             this.setData({
-               left: res.data,
-               content: res.data[this.data.nevid].medicine_set
+               left: data,
+               content: data[this.data.nevid].medicine_set,
+               list: list,
+               listCur: list[0]
             })
          }
       })
@@ -47,7 +100,7 @@ Page({
     * 生命周期函数--监听页面初次渲染完成
     */
    onReady: function () {
-
+      wx.hideLoading()
    },
 
    /**
